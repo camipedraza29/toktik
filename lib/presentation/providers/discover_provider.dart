02e9b99131861c3303b9a757ba08b2d725e9bba4
Camila Pedraza <cami.pedraza29@gmail.com>
2024-03-27 15:46:20 -0500

    repositorios y datasources

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,20 +1,28 @@
 
 import 'package:toktik/domain/entities/video_post.dart';
 import 'package:flutter/material.dart';
-import 'package:toktik/infrastructure/models/local_video_model.dart';
-import 'package:toktik/shared/data/loca_video_post.dart';
+import 'package:toktik/domain/repositories/video_post_repository.dart';
+
 
 class DiscoverProvider extends ChangeNotifier {
+
+  final VideoPostRepository videosRepository;
+  
   bool initialLoading = true;
   List<VideoPost> videos = [];
+
+  DiscoverProvider({
+    required this.videosRepository
+    });
   
 Future<void> loadNextPage() async{
 
 //await Future.delayed( const Duration(seconds:2));
 
-final List<VideoPost> newVideos = videoPosts.map(
-  (video) => LocalVideoModel.fromJson(video).toVideoPostEntity()
-  ).toList();
+//final List<VideoPost> newVideos = videoPosts.map(
+  //(video) => LocalVideoModel.fromJson(video).toVideoPostEntity()
+  //).toList();
+final newVideos = await videosRepository.getTrendingVideoByPage(1);
 
   videos.addAll(newVideos);
   initialLoading= false;
